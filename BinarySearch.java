4ca4539fa35cfe5a1d3df3eb3c7ee5314d230ca3
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 23:25:22 -0600

    Commit 1

---
+++ BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] datos = {1, 3, 5, 7, 9, 11, 13};
+        int resultado = busquedaBinaria(datos, 7);
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int busquedaBinaria(int[] arr, int objetivo) {
+        int inicio = 0;
+        int fin = arr.length - 1;
+
+        while (inicio <= fin) {
+            int medio = (inicio + fin) / 2;
+
+            if (arr[medio] == objetivo) {
+            }
+
+            if (arr[medio] < objetivo) {
+                inicio = medio + 1;
+            } else {
+                fin = medio - 1;
+            }
+        }
+        return -1;
+    }
+}
